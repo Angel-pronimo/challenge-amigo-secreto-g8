538fb45705e28f7396c611efaffd4ab487cca714
Angel Paz <paz.l.angel@gmail.com>
2025-02-03 23:55:37 -0300

    crear funcion amigo

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 //Decara un array vacío para guardar los nombres de los amigos
 let amigos = [];
+
+// Función para agregar un amigo a la lista
+function agregarAmigo() {
+    // Agregar amigos al array "amigos"
+    let nombre = document.getElementById("amigo").value;
+    amigos.push(nombre);
+    console.log(amigos)
+    }
