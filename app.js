6e62c21b0dd35edad01e1fb501ae71d862baac2b
Angel Paz <paz.l.angel@gmail.com>
2025-02-03 23:58:07 -0300

    crea funcion validar entrada

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 //Decara un array vacío para guardar los nombres de los amigos
 let amigos = [];
 
+// Función para validar si el casillero está vacío
+function validarEntrada() {
+    let nombre = document.getElementById("amigo").value.trim();
+    // Dar una alerta si el casillero está vacío
+    if (nombre.length === 0) {
+        alert("Por favor, inserte un nombre.");
+        return false;
+    }
+    return true;
+
 // Función para agregar un amigo a la lista
 function agregarAmigo() {
-    // Agregar amigos al array "amigos"
+    // Validar que el casillero no esté vacío
+    if (validarEntrada(true)) {
+    // Agregar una lista de amigos
     let nombre = document.getElementById("amigo").value;
     amigos.push(nombre);
-    console.log(amigos)
+    console.log(amigos);    
+    return;
     }
